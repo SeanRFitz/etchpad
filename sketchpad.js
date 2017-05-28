a7444b0c36bd9d16bf88617f3ffe12ab1bfe2a8c
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-05-27 20:06:00 -0500

    Fixed problems creating sketchboard (math-related problems)

--- sketchpad.js
+++ sketchpad.js
@@ -1,23 +1,24 @@
 $(document).ready( function() {
-	// Determine size of sketchpad (e.g 100x100) from user
-	var grid_height = $('#container').css('height');
-	grid_height = grid_height.replace("px", "");
-	var grid_width = $('#container').css('height');
-	grid_width = grid_height.replace("px", "");
-	var grid_size = grid_height * grid_width;
-	
-	var num_pixels = window.prompt("How many pixels do you want?");
 	// Create sketchpad
-	for (i=0; i <= num_pixels; i++) {
+	var num_pixels = window.prompt("How many pixels per side" + 
+		" do you want?");
+	for (i=0; i <= num_pixels * num_pixels; i++) {
     	jQuery('<div/>', {class: 'pixel',}).appendTo('#container');
     }
 
     // Determine size of pixels
-	var pixel_size = grid_size / num_pixels
-	var pixel_h = Math.sqrt(pixel_size);
-	var pixel_w = Math.sqrt(pixel_size);
-	// Add size of pixels to css class pixel
-    $('.pixel').css({'height':pixel_h, 'width':pixel_w});    	
-    
+    var grid_height = $('#container').css('height');
+	grid_height = grid_height.replace("px", "");
+	var pixel_size = grid_height / num_pixels;
 
+	// Add size of pixels to css class pixel
+    $('.pixel').css({'height':pixel_size, 'width':pixel_size});    	
+
+    // Change pixel color when mouse enters/exits pixel
+    $('.pixel').on('mouseenter', function() {
+    	$(this).css({'background-color':'red'})
+    });
+    $('.pixel').on('mouseleave', function() {
+    	$(this).css({'background-color':'gray'})
+    });
 });
