89a4d32e5df4c38bb6f95f30f030a85ce6ace2cd
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-05-27 19:31:25 -0500

    Added functionality to build pixel grid

--- sketchpad.js
+++ sketchpad.js
@@ -1,9 +1,21 @@
 $(document).ready( function() {
+	// Determine size of sketchpad (e.g 100x100) from user
+	var grid_height = $('#container').css('height');
+	grid_height = grid_height.replace("px", "");
+	var grid_width = $('#container').css('height');
+	grid_width = grid_height.replace("px", "");
+	var grid_size = grid_height * grid_width;
+	alert(grid_size);
+	// Determine size of pixels
+	var num_pixels = window.prompt("How many pixels do you want?");
+	var pixel_size = grid_size / num_pixels
+	var pixel_h = Math.sqrt(pixel_size);
+	var pixel_w = Math.sqrt(pixel_size);
+	// Create sketchpad based on input
     $('#container').on('click', function() {
-    	jQuery('<div/>', {
-    	  class: 'pixel',
-       }).appendTo('#container');
+    	$('.pixel').css({'height':pixel_h, 'width':pixel_w});
+    	for (i=0; i <= num_pixels; i++) {
+    		jQuery('<div/>', {class: 'pixel',}).appendTo('#container');
+    	}
     })
-
-
 });
